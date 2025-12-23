233401e9ee99672dbbd3d4e2a16b17635de18703
Maxi Barmetler <maxi.barmetler@gmail.com>
2025-12-23 13:26:32 +0100

    d16 parsing

---
+++ d16/d16.rs
@@ -0,0 +1,77 @@
+use nom::Parser;
+use nom::bytes::tag;
+use nom::character::complete::{alphanumeric1, char, space0};
+use nom::combinator::map;
+use nom::multi::separated_list0;
+use std::collections::HashMap;
+use std::str::FromStr;
+
+#[derive(Clone, Debug, Eq, PartialEq, Default)]
+struct Sue {
+    number: u32,
+    properties: HashMap<String, u32>,
+}
+
+impl FromStr for Sue {
+    type Err = nom::Err<nom::error::Error<String>>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        map(
+            (
+                tag("Sue"),
+                space0,
+                nom::character::complete::u32,
+                char(':'),
+                space0,
+                separated_list0(
+                    (char(','), space0),
+                    (
+                        alphanumeric1,
+                        char(':'),
+                        space0,
+                        nom::character::complete::u32,
+                    ),
+                ),
+            ),
+            |(_, _, number, _, _, props)| Sue {
+                number,
+                properties: props
+                    .into_iter()
+                    .map(|(name, _, _, v)| (str::to_string(name), v))
+                    .collect(),
+            },
+        )
+        .parse(s)
+        .map(|(_, o)| o)
+        .map_err(<nom::Err<nom::error::Error<&str>>>::to_owned)
+    }
+}
+
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        assert_eq!(
+            "Sue 69:".parse(),
+            Ok(Sue {
+                number: 69,
+                properties: HashMap::from([])
+            })
+        );
+        assert_eq!(
+            "Sue 69: perfumes: 7, vizslas: 9, akitas: 1".parse(),
+            Ok(Sue {
+                number: 69,
+                properties: HashMap::from([
+                    ("perfumes".to_string(), 7),
+                    ("vizslas".to_string(), 9),
+                    ("akitas".to_string(), 1),
+                ])
+            })
+        );
+    }
+}
